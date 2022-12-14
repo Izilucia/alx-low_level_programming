d91fbf412d6d572deb91a7b1baca044e1dfe4db9
root <luciabrakami@gmail.com>
2022-12-14 00:54:53 -0800

    5print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * main block
- *
- * Return 0
- */
- 
+ * main - print a c program using printf
+ * Return 0 if exites properly, non-zero otherwise
+*/
 int main(void)
 {
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
